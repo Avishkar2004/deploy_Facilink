422c4b25e8d3eca2672ec3d7ea52b6a085f8853f
Avishkar2004 <avishkarkakde2004@gmail.com>
2025-03-18 18:54:51 +0530

    Working Fine

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ app.use(
   cors({
     origin: [
       "https://facilink-server.vercel.app",
+      "https://facilink.in",
       "https://deploy-facilink-7sf1.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
